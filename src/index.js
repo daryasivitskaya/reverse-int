7b1fe6503ea9218dc46b13a709543afafe109827
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2024-08-01 19:19:20 +0200

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse (n) {   
+    const str = String(Math.abs(n));
+    let result = '';
+    for(let i = str.length - 1; i >= 0; i--){
+       result = result + str[i];
+    }
+    return Number(result);
 }
